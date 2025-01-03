16498de62dbe3f62a0fc3d45d5976c7fa31b2e93
Filipe Soccol <filipe.soccol@gmail.com>
2025-01-03 12:21:21 -0300

    Remove log on route chat.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -37,8 +37,6 @@ export async function POST(request: Request) {
     try {
         const { messages } = await request.json();
 
-        console.log(messages);
-
         const completion = await api.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
